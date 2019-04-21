3bcbef49d25d9ba5d5eac16225437f5287f79f35
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-04-21 17:44:33 -0600

    trying to finishing documentation

--- main.c
+++ main.c
@@ -7,12 +7,7 @@
 #include "read.c"
 #include <math.h>
 #include <time.h>
-<<<<<<< HEAD
-int cant;
-#define PTHREAD_COND_INITIALIZER {{0}}
-=======
  
-
 /* STATE_A = THREAD A runs next, STATE_B = THREAD B runs next */
 enum { STATE_A, STATE_B } state = STATE_A;
 pthread_cond_t      condA  = PTHREAD_COND_INITIALIZER;
@@ -27,7 +22,6 @@ int cantCarrosenPuente=0;
 int Esperando[2];
 pthread_cond_t EsteOeste[2];
 
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
 //datos de configuracion
 //Contador de carros creados en el este
 int vcontadorCarrosEste=0; 
@@ -71,31 +65,7 @@ int cant;
  int cant2;
 pthread_t OE;
 pthread_t EO;
-<<<<<<< HEAD
-//mutex para fuerza bruta 
-pthread_mutex_t fuerzabruta;
-pthread_t fb0;
-pthread_t fb1;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-void bloqueaf(){
-        pthread_mutex_lock(&fuerzabruta);
-}
-void desbloqueaf(){
-        pthread_mutex_unlock(&fuerzabruta);
-}
-void *fuerzabruta_method1(void *p ){
-bloqueaf();
-pthread_create(&OE, NULL, CCOE, NULL);
-desbloqueaf();
-}
-void *fuerzabruta_method2(void* p){
-bloqueaf();
-pthread_create(&OE, NULL, CCOE, NULL);
-desbloqueaf();
-}
-=======
 pthread_mutex_t  MonitorLock;
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
 void bloquea(int o);
 //arreglo ambulancias este-oeste
 int *arraye_o;
@@ -107,51 +77,17 @@ void desbloquearPuente();
 void * crearCarro(void * p);
 char * dir(int dir);
 
-// pthread_mutex_t fuerzabruta;
-// pthread_t fb0;
-// pthread_t fb1;
-// pthread_cond_t cond;
-// void bloqueaf(){
-//         pthread_mutex_lock(&fuerzabruta);
-// }
-// void desbloqueaf(){
-//         pthread_mutex_unlock(&fuerzabruta);
-// }
-// void *fuerzabruta_method1(void *p ){
-// bloqueaf();
-// pthread_create(&OE, NULL, CCOE, NULL);
-// desbloqueaf();
-// }
-// void* fuerzabruta_method2(void* p){
-// bloqueaf();
-// pthread_create(&EO, NULL, CCEO, NULL);
-// desbloqueaf();
-// }
-
 int main(){
 FILE* fp;
 char* filename="config.txt";
 read_txt(fp, filename, arreglo);
 arrayo_e=(int*)malloc(sizeof(int)*cantidadAmbulanciasOeste());
 arraye_o=(int*)malloc(sizeof(int)*cantidadAmbulanciasEste());
-<<<<<<< HEAD
-pthread_create(&OE, NULL, CCOE, NULL);
-pthread_create(&EO, NULL, CCEO, NULL);
-=======
 crearpuente();
 
 
 pthread_create(&OE, NULL,crearCarro , NULL);
-//pthread_create(&EO, NULL, CCEO, NULL);
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
 pthread_join(OE, NULL);
-pthread_join(EO, NULL);
-if (cant!= 0){
-        
-        pthread_cond_wait(&cond, &puente[arreglo[0]-1]);
-}else{
-        pthread_cond_signal(&cond);
-}
 
 }
 
@@ -160,10 +96,10 @@ float formula(int promedio){
         float ln= log(1-randomentre());
         return -promedio * ln;
 }
+
 float randomentre(){
         time_t t;
         srand((unsigned) time(&t));
-
 return ( (float) (rand()%10000) / 10000  );
 }
 
@@ -173,9 +109,7 @@ void crearpuente(){
         Esperando[0]=Esperando[1]=0;
         pthread_cond_init(&(EsteOeste[0]),NULL);
         pthread_cond_init(&(EsteOeste[1]),NULL);
-        
 for(int i=0; i<arreglo[0]; i++){
-//printf("%ld", arreglo[0]);
 pthread_mutex_init(&puente[i], NULL);
         }
 }
@@ -187,69 +121,16 @@ vcontadorCarrosEste++;
 bool esAmbulancia=false;
 int cantidadAmbulancias=cantidadAmbulanciasEste();
 for(int i=0;i<cantidadAmbulancias; i++){
-<<<<<<< HEAD
-        if(arrayo_e[i]==contadorCarrosEste){
-         esAmbulancia=true;
-         break;
-        }
-}     
-int modo= arreglo[1];
-int velocidad = calculavelocidad(arreglo[7],arreglo[]);
-pthread_mutex_lock(&puente[0]);
-int i=1;
-while(i<arreglo[0]){
-pthread_mutex_lock(&puente[i]);
-pthread_mutex_unlock(&puente[i-1]);
-if(esAmbulancia){
-        printf("soy ambulancia y voy por la posicion %d", i);
-}/*else {
-printf("soy carro y voy por la posicion %d",i);
-}*/
-printf("\n");
-time_t sec= (time_t) 1/velocidad;
-usleep(sec);
-i++;
-=======
         if(arraye_o[i]==vcontadorCarrosEste){
          esAmbulancia=true;
          break;
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
         }
 }     
-//int modo= arreglo[1];
 int velocidad = calculavelocidad(arreglo[7],arreglo[8]);
-// if(esAmbulancia)
-// printf("Soy una ambulanciaEO y vengo a esta velocidad:%d \n",velocidad);
-// else
-// printf("Soy un CarroEO y vengo a esta velocidad:%d \n",velocidad);
-//pthread_mutex_lock(&puente[0]);
-
-// int i=1;
-// while(i<arreglo[0]){
-// //printf("Bloqueando y Desbloqueando EO\n");        
-// pthread_mutex_unlock(&puente[i-1]);   
-// usleep(1);    
-// pthread_mutex_lock(&puente[i]);
-
-
-//time_t sec= (time_t) 1/velocidad;
-// if(esAmbulancia)
-// printf("Soy ambulanciaEO y voy por la posicion %d",i);
-
-// printf("\n");
-//usleep(sec);
-// i++;
-
-// }
 }
 
-<<<<<<< HEAD
-void* carroOE(void* p){    
-=======
 void* carroOE(void * p){
-//init_ambulanciasOE();  
-//printf("Creando carro OE\n");      
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
+//init_ambulanciasOE();       
 contadorCarrosOeste++;
 bool esAmbulancia=false;
 int cantidadAmbulancias=cantidadAmbulanciasOeste();
@@ -259,49 +140,11 @@ for(int i=0;i<cantidadAmbulancias; i++){
          break;
         }
 }     
-//int modo= arreglo[1];
 int velocidad = calculavelocidad(arreglo[14],arreglo[15]);
-<<<<<<< HEAD
-pthread_mutex_lock(&puente[0]);
-int i=1;
-while(i<arreglo[0]){
-pthread_mutex_lock(&puente[i]);
-pthread_mutex_unlock(&puente[i-1]);
-if(esAmbulancia){
-        printf("soy ambulancia y voy por la posicion %d", i);
-}/*else {
-printf("soy carro y voy por la posicion %d",i);
-}*/
-printf("\n");
-time_t sec= (time_t) 1/velocidad;
-usleep(sec);
-i++;
-}
-=======
-//pthread_mutex_lock(&puente[0]);
 if(esAmbulancia)
-printf("Soy una ambulanciaOE y vengo a esta velocidad:%d \n",velocidad);
+printf("Soy una ambulanciaOE y vengo a esta velocidad:%d \n", velocidad);
 else
-printf("Soy un CarroOE y vengo a esta velocidad:%d \n",velocidad);
-// int i=1;
-// while(i<arreglo[0]){   
-// if(mutex_is_locked())     
-// pthread_mutex_unlock(&puente[i-1]);  
-// usleep(1);
-// pthread_mutex_lock(&puente[i]);
-
-
-
-// //time_t sec= (time_t) 1/velocidad;
-// //usleep(sec);
-// i++;
-// if(esAmbulancia)
-// printf("Soy ambulancia y voy por la posicion %d",i);
-
-// printf("\n");
-// }
-
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
+printf("Soy un CarroOE y vengo a esta velocidad:%d \n", velocidad);
 }
 
 int calculavelocidad(int vmax, int vmin){
@@ -320,7 +163,6 @@ int cantidadAmbulanciasOeste(){
 }
 
 void probaAmbulancia(int cantidadAmbu,int totalCarros, int array[]){
- 
  srand(time(NULL));
  int rangoReal=(int)totalCarros/cantidadAmbu;
  int rangoVariable;
@@ -335,52 +177,7 @@ void probaAmbulancia(int cantidadAmbu,int totalCarros, int array[]){
   numero=(int)rand() % (rangoVariable-aux) + aux;
   array[i]=numero;
  }
- //printf("hola");
 }
-
-void* CCEO(void * p){
-       init_ambulanciasEO();
-        cant = arreglo[3];
-        while(cant!=0){
-                pthread_t carro;
-pthread_create(&carro, NULL, carroEO, NULL);
-<<<<<<< HEAD
-time_t sec= (time_t) 1/formula(arreglo[4]);
-usleep(sec);
-=======
-//time_t sec= (time_t) 1/(int)formula(arreglo[4]);
-//usleep(sec);
-//pthread_join(carro, NULL);
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
-cant--;
-}
-}
-
-void* CCOE(void* p){
-<<<<<<< HEAD
-       init_ambulanciasOE();
-        int cant = arreglo[11];
-        while(cant!=0){
-                pthread_t carro;
-pthread_create(&carro, NULL, carroOE, NULL);
-time_t sec= (time_t) 1/formula(arreglo[12]);
-usleep(sec);
-//pthread_join(carro, NULL);
-cant--;
-=======
-        init_ambulanciasOE();
-         cant2 = arreglo[11];
-        while(cant2!=0){
-                pthread_t carro;
-pthread_create(&carro, NULL, carroOE, NULL);
-//time_t sec= (time_t) 1/(int)formula(arreglo[12]);
-//usleep(sec);
-//pthread_join(carro, NULL);
-cant2--;
->>>>>>> e72794b545ff2dc27002b3ca970e04404f3dbc5e
-}
-}
-
 void init_ambulanciasEO(){
 probaAmbulancia(cantidadAmbulanciasEste(),arreglo[3], arraye_o);
 }
@@ -471,6 +268,7 @@ void* crearCarro(void *p)
         pthread_create(&carro, NULL, carroEO, NULL);
         salePuente(aleatorio);
          printf("El carro ha pasado el puente\n");
+         
         }
         else{
         printf("Carro entrando al puente en direccion %s\n",dir(1));
@@ -492,120 +290,3 @@ char * dir(int dir){
         
         
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  void bloquea(int o){
-// pthread_mutex_unlock(&puente[o-1]);
-// pthread_mutex_lock(&puente[o]);
-// printf("soy carro y voy por la posicion %d",o);
-//  }
-
-
-/*
-
-void* CCEO(void * p){
-       // printf("Funcion CCEO\n");
-       init_ambulanciasEO();
-        cant = arreglo[3];
-        while(cant!=0){
-
-  
-  for(int i=0; i<10; i++){
-        printf("Bloqueando sentido EO\n");
-        pthread_mutex_lock(&puente[i]);
-        while (state != STATE_A){
-        pthread_cond_wait(&condA, &puente[i]);
-          printf("Esperanto sentido EO\n");
-        }
-        pthread_mutex_unlock(&puente[i]);
-  }
-                pthread_t carro;
-
-pthread_create(&carro, NULL, carroEO, NULL);
-time_t sec= (time_t) 1/formula(arreglo[4]);
-pthread_join(carro,NULL);
-usleep(sec);
-
-for(int i=0;i<10;i++){
-printf("Desbloqueando sentido EO\n");
-  pthread_mutex_lock(&puente[i]);
-        state = STATE_B;
-        //pthread_cond_signal(&condB);
-        pthread_cond_broadcast(&condB);
-        pthread_mutex_unlock(&puente[i]);
-}
-//pthread_join(carro, NULL);
-cant--;
-}
-}
-
-
-void* CCOE(void* p){
-        //printf("Funcion CCOE\n");
-        init_ambulanciasOE();
-        cant2 = arreglo[11];
-        while(cant2!=0){
-          
-                 for(int i=0;i<10;i++){
-        printf("Bloqueando sentido OE\n");
-        pthread_mutex_lock(&puente[i]);
-        while (state != STATE_B){
-            pthread_cond_wait(&condB, &puente[i]);
-            printf("Esperanto sentido OE\n");
-            }
-        pthread_mutex_unlock(&puente[i]);
-                 }
-
-//Do stuff
-                pthread_t carro;
-pthread_create(&carro, NULL, carroOE, NULL);
-time_t sec= (time_t) 1/formula(arreglo[12]);
-usleep(sec);
-pthread_join(carro,NULL);
-
-
- for(int i=0;i<10;i++){
-         printf("Desbloqueando sentido OE\n");
-        pthread_mutex_lock(&puente[i]);
-        state = STATE_A;
-        pthread_cond_broadcast(&condA);
-        pthread_mutex_unlock(&puente[i]);
-        
- }
-cant2--;
-}
-}
-
-*/
